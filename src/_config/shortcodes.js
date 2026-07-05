22d63a9d260371e3245d766b89f422b4693f8860
Colin Johnston <dev@colinjohnston.com>
2026-07-04 18:48:50 -0700

    adds config dir with svg shortcode

---
+++ src/_config/shortcodes.js
@@ -0,0 +1,7 @@
+// https://github.com/madrilene/eleventy-excellent/blob/main/src/_config/shortcodes.js
+
+// import {imageShortcode, imageKeysShortcode} from './shortcodes/image.js';
+import { svgShortcode } from './shortcodes/svg.js';
+
+// export default {imageShortcode, imageKeysShortcode, svgShortcode};
+export default { svgShortcode };
